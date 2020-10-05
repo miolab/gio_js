b9dc7f88e0505273cf59106bab86215aa4e6529f
im <poson_hemp@yahoo.co.jp>
2020-10-05 13:53:00 +0900

    update init country JP & add mock data flow

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Gio.js
 
-https://giojs.org/
+- Official https://giojs.org/
+
+- GitHub https://github.com/syt123450/giojs
+
+- 国名コード https://ja.wikipedia.org/wiki/ISO_3166-1
--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 const container = document.getElementById("globeArea");
 const controller = new GIO.Controller(container);
+
+controller.setInitCountry('JP');
+
+const data = [{
+    "e": "JP",
+    "i": "BD",
+    "v": 2000
+  },
+  {
+    "e": "BR",
+    "i": "JP",
+    "v": 1000
+  }
+];
+
 controller.addData(data);
 controller.init();
